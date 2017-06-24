fea982cc01dc761f537603a1307beec68d5262bf
Yao Min <ngyaomin@ymail.com>
2017-06-24 21:26:15 +0800

    Animated to allow dragging of cards

--- src/Ball.js
+++ src/Ball.js
@@ -13,7 +13,7 @@ class Ball extends Component {
     return (
       <Animated.View style={this.position.getLayout()}>
         <View style={styles.ball} />
-      </Animated.View>
+      </Animiated.View>
     );
   }
 }
--- src/Deck.js
+++ src/Deck.js
@@ -10,15 +10,16 @@ class Deck extends Component {
   constructor(props) {
     super(props);
 
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        console.log(gesture);
+        position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       onPanResponderRelease: () => {}
     });
 
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
 
   renderCards() {
@@ -29,9 +30,12 @@ class Deck extends Component {
 
   render() {
     return (
-      <View {...this.state.panResponder}>
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
