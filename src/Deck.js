0910d18e06a48cfe3f34420df16242dc0a37a3ee
Yao Min <ngyaomin@ymail.com>
2017-06-24 20:39:32 +0800

    Set up pan resposnder

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import {
+  View,
+  Animated,
+  PanResponder
+  } from 'react-native';
+
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => false,
+      onPanResponderMove: () => {},
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder };
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
